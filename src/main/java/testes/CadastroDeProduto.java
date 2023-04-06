e5529bca41596aba6bea58fb554ec15d70f963cd
HENRIQUE G PREZOTTI <henriqueprezotti@gmail.com>
2023-04-06 17:25:26 -0300

    Configurations and EntitManager

---
+++ src/main/java/testes/CadastroDeProduto.java
@@ -0,0 +1,26 @@
+package testes;
+
+import modelo.Produto;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.math.BigDecimal;
+
+public class CadastroDeProduto {
+
+    public static void main(String[] args) {
+        Produto celular = new Produto();
+        celular.setNome("Xiaomi Redmi");
+        celular.setDescricao("Muito Legal");
+        celular.setPreco(new BigDecimal("800"));
+
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("loja");
+        EntityManager em = factory.createEntityManager();
+        em.getTransaction().begin();
+        em.persist(celular);
+        em.getTransaction().commit();
+        em.close();
+    }
+
+}
