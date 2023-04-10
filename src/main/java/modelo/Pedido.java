4e145624787c755cba82468a9bca608935ef5e18
HENRIQUE G PREZOTTI <henriqueprezotti@gmail.com>
2023-04-09 21:03:54 -0300

    Pedido Entity mapped

---
+++ src/main/java/modelo/Pedido.java
@@ -0,0 +1,61 @@
+package modelo;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "pedidos")
+public class Pedido {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDate data = LocalDate.now();
+
+    @Column(name = "valor_total")
+    private BigDecimal valorTotal;
+
+    @ManyToOne
+    private Cliente cliente;
+
+    @OneToMany(mappedBy = "pedido", cascade = CascadeType.ALL)
+    private List<ItemPedido> itens = new ArrayList<>();
+
+    public Pedido() {
+    }
+
+    public Pedido(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public void adicionarItem(ItemPedido item){
+        item.setPedido(this);
+        this.itens.add(item);
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public BigDecimal getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(BigDecimal valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+}
